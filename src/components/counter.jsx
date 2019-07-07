7c6db6daf27b4965ed3eaf7a684c5a955c27aa80
Frontend Neobis <you@example.com>
2019-07-07 03:51:44 -0700

    React Counter App with Mosh

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,31 +1,25 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    state = {
-        count: 0
-    }
 
-    styles = {
-        fontSize: 30,
-        fontWeight: "bold"
-    }
     render() { 
         return (
-        <React.Fragment>
-            <span  style={this.styles} className = {this.getBadgeClasses()}>{this.formatCount()}</span>
-            <button className="btn btn-secondary btn-sm">Increment</button>
-        </React.Fragment>);
+        <div>
+            <span  className = {this.getBadgeClasses()}>{this.formatCount()}</span>
+            <button onClick={() => this.props.onIncrement(this.props.counter)} className="btn btn-secondary btn-sm">Increment</button>
+            <button className="btn btn-danger btn-sm m-2" onClick={() =>this.props.onDelete(this.props.counter.id)}>Delete</button>
+        </div>);
     }
 
 //rendering classes dynamically
 getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
 }
 
 formatCount() {
-    const { count } = this.state; //in order to not repaet "this.state.count"
+    const { value: count } = this.props.counter; //in order to not repaet "this.state.count"
     return count === 0 ? "Zero" : count;
 };    
 }
